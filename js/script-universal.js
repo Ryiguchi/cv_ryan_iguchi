0b89b42c5e81b80cf75d369236d2422aba67b777
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-10-27 13:01:50 +0200

    fixed links in header

--- js/script-universal.js
+++ js/script-universal.js
@@ -26,3 +26,9 @@ navBtn.addEventListener("click", function (e) {
   e.preventDefault();
   mobileNav.classList.toggle("hidden");
 });
+
+mobileNav.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (!e.target.classList.contains("mobile-nav--link")) return;
+  mobileNav.classList.add("hidden");
+});
