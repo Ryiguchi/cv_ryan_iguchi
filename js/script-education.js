ca3cc548f2922b1d50aad72378bc5b76588cda9f
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-11-03 23:04:19 +0100

    Read all education info from JSON

--- js/script-education.js
+++ js/script-education.js
@@ -1,49 +1,192 @@
 "use strict";
 
-//////////////////////////////////////////////
-// SELECTORS /////////////////////////////////
-//////////////////////////////////////////////
+const loadFunctionality = function () {
+  // SELECTORS /////////////////////////////////
+  const tabsContainerEd = document.querySelector(".tabs-container-ed");
+  const tabContainers = document.querySelectorAll(".tabs-container");
+  const tabsEd = document.querySelectorAll(".tab-ed");
+  const tabsSkill = document.querySelectorAll(".tab-skill");
+  const tabsContentEd = document.querySelectorAll(".tab-content-ed");
+  const tabsContentSkill = document.querySelectorAll(".tab-content-skill");
+  const navSection = document.querySelector(".nav-section");
 
-// Elements
-const tabsContainerEd = document.querySelector(".tabs-container-ed");
-const tabsContainerSkill = document.querySelector(".tabs-container-skill");
-const tabContainers = document.querySelectorAll(".tabs-container");
-const tabsEd = document.querySelectorAll(".tab-ed");
-const tabsSkill = document.querySelectorAll(".tab-skill");
-const tabsContentEd = document.querySelectorAll(".tab-content-ed");
-const tabsContentSkill = document.querySelectorAll(".tab-content-skill");
-const navSection = document.querySelector(".nav-section");
+  // FUNCTIONS /////////////////////////////////
+  // TABBED COMPONENT //
+  const changeActiveTab = function (clicked, section) {
+    document.querySelectorAll(`.tab-${section}`).forEach((tab) => {
+      tab.classList.remove("tab-active");
+    });
+    clicked.classList.add("tab-active");
 
-// TABBED COMPONENT //
+    document.querySelectorAll(`.tab-content-${section}`).forEach((tab) => {
+      tab.classList.add("hidden");
+    });
+    document
+      .querySelector(`.tab-content-${section}-${clicked.dataset.num}`)
+      .classList.remove("hidden");
+  };
 
-// FUNCTIONS /////////////////////////////////
-
-const changeActiveTab = function (clicked, section) {
-  document.querySelectorAll(`.tab-${section}`).forEach((tab) => {
-    tab.classList.remove("tab-active");
+  // Event Handlers /////////////////////////////////
+  tabContainers.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      const clicked = e.target.closest(".tab");
+      if (!clicked || clicked.classList.contains("tab-active")) return;
+      const section = clicked.classList.contains("tab-ed") ? "ed" : "skill";
+      changeActiveTab(clicked, section);
+    });
   });
-  clicked.classList.add("tab-active");
 
-  document.querySelectorAll(`.tab-content-${section}`).forEach((tab) => {
-    tab.classList.add("hidden");
+  navSection.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = e.target.closest("a").getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
-  document
-    .querySelector(`.tab-content-${section}-${clicked.dataset.num}`)
-    .classList.remove("hidden");
 };
 
-// Event Handlers /////////////////////////////////
-tabContainers.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    const clicked = e.target.closest(".tab");
-    if (!clicked || clicked.classList.contains("tab-active")) return;
-    const section = clicked.classList.contains("tab-ed") ? "ed" : "skill";
-    changeActiveTab(clicked, section);
-  });
-});
+const certificatesTab = document.querySelector(".tab-4-ed");
+const tabsContainerSkill = document.querySelector(".tabs-container-skill");
+const skillsSection = document.querySelector(".skills");
+const tabsContainerOuter = document.querySelector(".tabs-container-outer");
+const tabContentUdemy = document.querySelector(".tab-content-header-udemy");
 
-navSection.addEventListener("click", (e) => {
-  e.preventDefault();
-  const id = e.target.closest("a").getAttribute("href");
-  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-});
+const createHtmlEd = function (schools) {
+  let htmlTab = "";
+  let htmlContent = "";
+  let htmlSubjects1;
+  let htmlSubjects2;
+  schools.forEach((school, i) => {
+    htmlSubjects1 = "";
+    htmlSubjects2 = "";
+    school.subjects.forEach((sub, i) => {
+      i <= school.subjects.length / 2
+        ? (htmlSubjects1 += `<li>${sub}</li>`)
+        : (htmlSubjects2 += `<li>${sub}</li>`);
+    });
+
+    htmlTab += `
+    <div class="tab tab-ed tab-${i + 1}-ed ${
+      i === 0 ? "tab-active" : ""
+    }" data-num="${i + 1}">
+    <div class="tab-pointer"></div>
+    <p class="tab-ed-name">${school.name.nameTab}</p>
+    <p class="tab-ed-date">${school.date}</p>
+    </div>
+    `;
+
+    htmlContent += `
+    <div class="tab-content-ed tab-content tab-content-ed-${i + 1} ${
+      i === 0 ? "" : "hidden"
+    }">
+      <div class="tab-content-header">
+        <div class="tab-content-header-text">
+          <h2>${school.name.nameFull}</h2>
+          <h4>${school.location}</h4>
+          <h4 class="tab-content-date">${school.date}</h4>
+          <p>Major: ${school.major}</p>
+        </div>
+        <img
+          src="${school.img.src}"
+          alt="${school.img.alt}"
+          class="logo-${school.img.class} logo"
+        />
+      </div>
+      <div class="tab-content-description">${school.description}</div>
+      <div class="tab-content-subjects">
+        <h4>Subjects Studied</h4>
+        <ul class="education-ul">
+          ${htmlSubjects1}
+        </ul>
+        <ul>
+          ${htmlSubjects2}
+        </ul>
+      </div>
+    </div>
+    `;
+  });
+  certificatesTab.insertAdjacentHTML("beforebegin", htmlTab);
+  tabsContainerOuter.insertAdjacentHTML("afterend", htmlContent);
+};
+
+const createHtmlCer = function (certificates) {
+  let htmlContent = "";
+
+  certificates.forEach((cert) => {
+    htmlContent += `
+        <div class="tab-content-header-text">
+          <h4>${cert.courseName}</h4>
+          <h4>${cert.hours} hours</h4>
+          <p>${cert.certificateType}</p>
+        </div>
+    `;
+  });
+  tabContentUdemy.insertAdjacentHTML("beforeend", htmlContent);
+};
+
+const createHtmlSkills = function (cats) {
+  let htmlTabs = "";
+  let htmlContent = "";
+  let htmlSkill;
+  cats.forEach((cat, i) => {
+    htmlSkill = "";
+    // content skills
+    cat.subjects.forEach((sub) => {
+      htmlSkill += `
+      <li>
+      <span class="skills-list--skill">${sub.name}</span
+      ><span class="education-print">${sub.print}</span>
+      <div class="skills-list-background">
+      <div class="skills-${sub.name.toLowerCase()} skills-bar"></div>
+      </div>
+      </li>
+      `;
+    });
+
+    htmlContent += `
+    <div class="tab-content tab-content-skill tab-content-skill-${i + 1} ${
+      i === 0 ? "" : "hidden"
+    }">
+      <ul class="skills-list education-ul">
+        ${htmlSkill}
+      </ul>
+    </div>
+    `;
+
+    // tabs skills
+    htmlTabs += `
+    <div class="tab tab-skill tab-${i + 1}-ed ${
+      i === 0 ? "tab-active" : ""
+    }" data-num="${i + 1}">
+      <div class="tab-pointer"></div>
+      <p class="tab-ed-name">${cat.category}</p>
+    </div>
+    `;
+  });
+  tabsContainerSkill.insertAdjacentHTML("beforeend", htmlTabs);
+  skillsSection.insertAdjacentHTML("beforeend", htmlContent);
+};
+
+const errorLoading = function (err) {
+  document.querySelector(".schools").classList.add("hidden");
+  document.querySelector(".skills").classList.add("hidden");
+  const errorEl = document.querySelector(".error");
+  errorEl.classList.remove("hidden");
+  errorEl.textContent = err.message;
+};
+
+const loadData = async function () {
+  try {
+    const res = await fetch("/json/education.json");
+    if (!res.ok)
+      throw new Error("💥 There was a problem retrieving the data 💥");
+    const [{ education: edu }, { certificates: cert }, { skills }] =
+      await res.json();
+    createHtmlEd(edu);
+    createHtmlCer(cert);
+    createHtmlSkills(skills);
+    loadFunctionality();
+  } catch (err) {
+    errorLoading(err);
+  }
+};
+
+loadData();
--- js/script-resume.js
+++ js/script-resume.js
@@ -2,10 +2,12 @@
 
 // SELECTORS /////////////////////////////////
 
-const jobSections = document.querySelectorAll(".section-job");
+const jobsContainer = document.querySelector(".experience");
+const h1 = document.querySelector("h1");
 
 // FUNCTIONS ////////////////////////////////
 
+// reveals jobs
 const revealJob = function (entries, observer) {
   const [entry] = entries;
 
@@ -15,11 +17,67 @@ const revealJob = function (entries, observer) {
   observer.unobserve(entry.target);
 };
 
+// Intersection Observer
 const sectionObserver = new IntersectionObserver(revealJob, {
   root: null,
   threshold: 0.8,
 });
 
-jobSections.forEach((job) => {
-  !job.classList.contains("wine") && sectionObserver.observe(job);
-});
+// Build and insert HTML for jobs
+const appendJobs = function (data) {
+  data.forEach((job) => {
+    let res = "";
+    job.responsibilities.forEach((r) => (res += `<li>${r}</li>`));
+
+    let html = `
+    <section class="${job.className} job job-${job.side} section-job ${
+      job.className === "wine" ? "" : "hidden-section"
+    }">
+      <h4 class="date">${job.date}</h4>
+      <h3 class="company">${job.name}</h3>
+      <h2 class="job-title">${job.title}</h2>
+      <p class="job-description">${job.description}</p>
+
+      <!-- DETAILS -->
+
+      <details>
+        <h5 class="resume-h5">Responsibilities and Achievements</h5>
+        <ul>
+        ${res}
+        </ul>
+      </details>
+
+      <!-- PRINT FORMAT -->
+
+      <div class="print">
+        <h5 class="resume-h5">Responsibilities and Achievements</h5>
+        <ul>
+          ${res}
+        </ul>
+      </div>
+    </section>
+    `;
+    jobsContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+// Get job data and set observer
+const getJobs = async function () {
+  try {
+    const res = await fetch("/json/work.json");
+    if (!res.ok)
+      throw new Error("💥 There was a problem loading the resume 💥");
+    const { jobs: data } = await res.json();
+    appendJobs(data);
+
+    // set up reveal effect on newly created elements
+    const jobSections = document.querySelectorAll(".section-job");
+    jobSections.forEach((job) => {
+      !job.classList.contains("wine") && sectionObserver.observe(job);
+    });
+  } catch (err) {
+    h1.textContent = err.message;
+  }
+};
+
+getJobs();
