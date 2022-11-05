3b58b0711604736f63237208118454eb52a00654
Ryan Iguchi <ryan.iguchi1@gmail.com>
2022-11-05 17:40:44 +0100

    fixed json link

--- js/script-education.js
+++ js/script-education.js
@@ -1,53 +1,53 @@
-"use strict";
-const tabContainers = document.querySelectorAll(".tabs-container");
-const navSection = document.querySelector(".nav-section");
-const certificatesTab = document.querySelector(".tab-4-ed");
-const tabsContainerSkill = document.querySelector(".tabs-container-skill");
-const skillsSection = document.querySelector(".skills");
-const tabsContainerOuter = document.querySelector(".tabs-container-outer");
-const tabContentUdemy = document.querySelector(".tab-content-header-udemy");
+'use strict';
+const tabContainers = document.querySelectorAll('.tabs-container');
+const navSection = document.querySelector('.nav-section');
+const certificatesTab = document.querySelector('.tab-4-ed');
+const tabsContainerSkill = document.querySelector('.tabs-container-skill');
+const skillsSection = document.querySelector('.skills');
+const tabsContainerOuter = document.querySelector('.tabs-container-outer');
+const tabContentUdemy = document.querySelector('.tab-content-header-udemy');
 
 //////////////////////////////////////////////////////////
 // PAGE FUNCTIONALITY  ///////////////////////////////////
 //////////////////////////////////////////////////////////
 const loadFunctionality = function () {
   // SELECTORS /////////////////////////////////
-  const tabsContainerEd = document.querySelector(".tabs-container-ed");
-  const tabsEd = document.querySelectorAll(".tab-ed");
-  const tabsSkill = document.querySelectorAll(".tab-skill");
-  const tabsContentEd = document.querySelectorAll(".tab-content-ed");
-  const tabsContentSkill = document.querySelectorAll(".tab-content-skill");
+  const tabsContainerEd = document.querySelector('.tabs-container-ed');
+  const tabsEd = document.querySelectorAll('.tab-ed');
+  const tabsSkill = document.querySelectorAll('.tab-skill');
+  const tabsContentEd = document.querySelectorAll('.tab-content-ed');
+  const tabsContentSkill = document.querySelectorAll('.tab-content-skill');
 
   // FUNCTIONS /////////////////////////////////
   // TABBED COMPONENT //
   const changeActiveTab = function (clicked, section) {
-    document.querySelectorAll(`.tab-${section}`).forEach((tab) => {
-      tab.classList.remove("tab-active");
+    document.querySelectorAll(`.tab-${section}`).forEach(tab => {
+      tab.classList.remove('tab-active');
     });
-    clicked.classList.add("tab-active");
+    clicked.classList.add('tab-active');
 
-    document.querySelectorAll(`.tab-content-${section}`).forEach((tab) => {
-      tab.classList.add("hidden");
+    document.querySelectorAll(`.tab-content-${section}`).forEach(tab => {
+      tab.classList.add('hidden');
     });
     document
       .querySelector(`.tab-content-${section}-${clicked.dataset.num}`)
-      .classList.remove("hidden");
+      .classList.remove('hidden');
   };
 
   // Event Handlers /////////////////////////////////
-  tabContainers.forEach((el) => {
-    el.addEventListener("click", (e) => {
-      const clicked = e.target.closest(".tab");
-      if (!clicked || clicked.classList.contains("tab-active")) return;
-      const section = clicked.classList.contains("tab-ed") ? "ed" : "skill";
+  tabContainers.forEach(el => {
+    el.addEventListener('click', e => {
+      const clicked = e.target.closest('.tab');
+      if (!clicked || clicked.classList.contains('tab-active')) return;
+      const section = clicked.classList.contains('tab-ed') ? 'ed' : 'skill';
       changeActiveTab(clicked, section);
     });
   });
 
-  navSection.addEventListener("click", (e) => {
+  navSection.addEventListener('click', e => {
     e.preventDefault();
-    const id = e.target.closest("a").getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    const id = e.target.closest('a').getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   });
 };
 
@@ -57,13 +57,13 @@ const loadFunctionality = function () {
 
 const loadData = function () {
   const createHtmlEd = function (schools) {
-    let htmlTab = "";
-    let htmlContent = "";
+    let htmlTab = '';
+    let htmlContent = '';
     let htmlSubjects1;
     let htmlSubjects2;
     schools.forEach((school, i) => {
-      htmlSubjects1 = "";
-      htmlSubjects2 = "";
+      htmlSubjects1 = '';
+      htmlSubjects2 = '';
       school.subjects.forEach((sub, i) => {
         i <= school.subjects.length / 2
           ? (htmlSubjects1 += `<li>${sub}</li>`)
@@ -72,7 +72,7 @@ const loadData = function () {
 
       htmlTab += `
     <div class="tab tab-ed tab-${i + 1}-ed ${
-        i === 0 ? "tab-active" : ""
+        i === 0 ? 'tab-active' : ''
       }" data-num="${i + 1}">
     <div class="tab-pointer"></div>
     <p class="tab-ed-name">${school.name.nameTab}</p>
@@ -82,7 +82,7 @@ const loadData = function () {
 
       htmlContent += `
     <div class="tab-content-ed tab-content tab-content-ed-${i + 1} ${
-        i === 0 ? "" : "hidden"
+        i === 0 ? '' : 'hidden'
       }">
       <div class="tab-content-header">
         <div class="tab-content-header-text">
@@ -110,14 +110,14 @@ const loadData = function () {
     </div>
     `;
     });
-    certificatesTab.insertAdjacentHTML("beforebegin", htmlTab);
-    tabsContainerOuter.insertAdjacentHTML("afterend", htmlContent);
+    certificatesTab.insertAdjacentHTML('beforebegin', htmlTab);
+    tabsContainerOuter.insertAdjacentHTML('afterend', htmlContent);
   };
 
   const createHtmlCer = function (certificates) {
-    let htmlContent = "";
+    let htmlContent = '';
 
-    certificates.forEach((cert) => {
+    certificates.forEach(cert => {
       htmlContent += `
         <div class="tab-content-header-text">
           <h4>${cert.courseName}</h4>
@@ -126,17 +126,17 @@ const loadData = function () {
         </div>
     `;
     });
-    tabContentUdemy.insertAdjacentHTML("beforeend", htmlContent);
+    tabContentUdemy.insertAdjacentHTML('beforeend', htmlContent);
   };
 
   const createHtmlSkills = function (cats) {
-    let htmlTabs = "";
-    let htmlContent = "";
+    let htmlTabs = '';
+    let htmlContent = '';
     let htmlSkill;
     cats.forEach((cat, i) => {
-      htmlSkill = "";
+      htmlSkill = '';
       // content skills
-      cat.subjects.forEach((sub) => {
+      cat.subjects.forEach(sub => {
         htmlSkill += `
       <li>
       <span class="skills-list--skill">${sub.name}</span
@@ -150,7 +150,7 @@ const loadData = function () {
 
       htmlContent += `
     <div class="tab-content tab-content-skill tab-content-skill-${i + 1} ${
-        i === 0 ? "" : "hidden"
+        i === 0 ? '' : 'hidden'
       }">
       <ul class="skills-list education-ul">
         ${htmlSkill}
@@ -161,30 +161,32 @@ const loadData = function () {
       // tabs skills
       htmlTabs += `
     <div class="tab tab-skill tab-${i + 1}-ed ${
-        i === 0 ? "tab-active" : ""
+        i === 0 ? 'tab-active' : ''
       }" data-num="${i + 1}">
       <div class="tab-pointer"></div>
       <p class="tab-ed-name">${cat.category}</p>
     </div>
     `;
     });
-    tabsContainerSkill.insertAdjacentHTML("beforeend", htmlTabs);
-    skillsSection.insertAdjacentHTML("beforeend", htmlContent);
+    tabsContainerSkill.insertAdjacentHTML('beforeend', htmlTabs);
+    skillsSection.insertAdjacentHTML('beforeend', htmlContent);
   };
 
   const errorMessage = function (err) {
-    document.querySelector(".schools").classList.add("hidden");
-    document.querySelector(".skills").classList.add("hidden");
-    const errorEl = document.querySelector(".error");
-    errorEl.classList.remove("hidden");
+    document.querySelector('.schools').classList.add('hidden');
+    document.querySelector('.skills').classList.add('hidden');
+    const errorEl = document.querySelector('.error');
+    errorEl.classList.remove('hidden');
     errorEl.textContent = err.message;
   };
 
   const fetchData = async function () {
     try {
-      const res = await fetch("/json/education.json");
+      const res = await fetch(
+        'https://github.com/Ryiguchi/cv-javascript/blob/ca3cc548f2922b1d50aad72378bc5b76588cda9f/json/education.json'
+      );
       if (!res.ok)
-        throw new Error("💥 There was a problem retrieving the data 💥");
+        throw new Error('💥 There was a problem retrieving the data 💥');
       const [{ education: edu }, { certificates: cert }, { skills }] =
         await res.json();
       createHtmlEd(edu);
